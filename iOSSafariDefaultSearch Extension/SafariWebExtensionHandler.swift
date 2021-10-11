035bfcc16e69ce2f33057fb7cede85d9e021ba3f
guan-kevin <github@pigzy.net>
2021-10-10 22:10:58 -0700

    Initial Commit

---
+++ iOSSafariDefaultSearch Extension/SafariWebExtensionHandler.swift
@@ -0,0 +1,24 @@
+//
+//  SafariWebExtensionHandler.swift
+//  iOSSafariDefaultSearch Extension
+//
+//  Created by Kevin Guan on 10/10/21.
+//
+
+import SafariServices
+import os.log
+
+class SafariWebExtensionHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let item = context.inputItems[0] as! NSExtensionItem
+        let message = item.userInfo?[SFExtensionMessageKey]
+        os_log(.default, "Received message from browser.runtime.sendNativeMessage: %@", message as! CVarArg)
+
+        let response = NSExtensionItem()
+        response.userInfo = [ SFExtensionMessageKey: [ "Response to": message ] ]
+
+        context.completeRequest(returningItems: [response], completionHandler: nil)
+    }
+
+}
---
+++ iOSSafariDefaultSearch/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  iOSSafariDefaultSearch
+//
+//  Created by Kevin Guan on 10/10/21.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
