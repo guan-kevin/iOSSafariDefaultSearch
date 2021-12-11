8d3880231c093def6c824c80dc0d245fbfc1eae7
guan-kevin <github@pigzy.net>
2021-12-10 18:45:03 -0800

    Upload Project

--- iOSSafariDefaultSearch Extension/Resources/content.js
+++ iOSSafariDefaultSearch Extension/Resources/content.js
@@ -1,7 +1,3 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("Received response: ", response);
-});
-
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
-});
+if (document.location.hostname === 'www.ecosia.org') {
+    document.location.hostname = "www.startpage.com"
+}
